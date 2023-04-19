46b9abd97b35c6baa1368c5c1b55cbcfc9324a57
vyshali <vyshali.ad20@bitsathy.ac.in>
2023-04-20 00:34:44 +0530

    Completed 'update flight'

--- routes/flights.js
+++ routes/flights.js
@@ -22,6 +22,18 @@ catch (ex){
 }
 })
 
+routes.get("/:id", async(req,res)=>{
+try{
+    let flight = await Flights.find({_id:req.params.id})
+    .populate("allotmentId bookedUsers")
+    if(flight.length<1) return res.send({message:"Invalid Flight ID"})
+    res.send(flight)
+}
+catch(ex){
+    res.status(400).send(_.pick(ex,["message"]))
+}
+} )
+
 routes.post("/",async(req,res)=>{
 try{
     // req.body.seats = JSON.parse(req.body.seats)
@@ -31,6 +43,27 @@ try{
     let fligt = createFlight(req.body.name,req.body.number,req.body.pricePerSeat,req.body.seats)
     fligt = await fligt.save();
     res.send(fligt)
+}
+catch(ex){
+    res.status(400).send(_.pick(ex,["message"]))
+}
+})
+
+routes.put("/:id",async(req,res)=>{
+try{
+    let flight = await Flights.findById(req.params.id);
+    if(!flight) return res.status(404).send({"message":"Flight Not Found / invalid flight id"});
+    
+    const {error} = flightJoiSchema.validate(req.body) 
+    if(error) return res.status(400).send(_.pick(error,["message"]));
+ 
+    flight.name = req.body.name;
+    flight.number = req.body.number;
+    flight.pricePerSeat = req.body.pricePerSeat;
+    flight.seats = req.body.seats;
+
+    let result = await flight.save();
+    res.send(result)
 }
 catch(ex){
     res.status(400).send(_.pick(ex,["message"]))
@@ -65,7 +98,7 @@ try{
     if(flight.bookedUsers.includes(req.body.userId)) return res.send(flight);
     flight.bookedUsers.push(user)
     let result = await flight.save()
-    
+
     res.send(result)
 }
 catch(ex){
