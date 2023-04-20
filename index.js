6009801fac3d7344e9df8711af50549da2a4865b
vyshali <vyshali.ad20@bitsathy.ac.in>
2023-04-21 01:10:03 +0530

    included cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose")
+const cors = require('cors')
 const app = express();
+app.use(cors())
 
 const userRoute = require("./routes/users") 
 const flightsRoutes = require("./routes/flights")
